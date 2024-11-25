a46c22da8f1aeb9478edeb01f6d009bd366e3592
MiesieduoCodes <miesieduoveria@gmail.com>
2024-11-25 14:41:01 +0100

    vvv

--- src/Constants/MovieCollection.js
+++ src/Constants/MovieCollection.js
@@ -7,35 +7,35 @@ const movieData = [
     },
     {
       title: "Avengers Assemble",
-      backgroundImage: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/5131/795131-i",
+      backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyIQjgTv6hbEpesus_2S_t0WBt5mGknk8kA&s",
       synopsis: "Earth's Mightiest Heroes come together to save the world from various threats.",
 
     },
     {
       title: "Scooby-Doo, Where Are You!",
-      backgroundImage: "https://example.com/images/scooby-doo.jpg",
+      backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcsh78_5iG-q54nNUSW9eNk6j8gKIjXR8MIw&s",
       synopsis: "Scooby-Doo and the gang solve mysteries involving supposedly supernatural creatures.",
     },
     {
       title: "Barbie: Dreamhouse Adventures",
-      backgroundImage: "https://example.com/images/barbie.jpg",
+      backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRtnE-Hocdjv-a-XwLV2RGogb7oWlIc91gBg&s",
       synopsis: "Barbie and her friends embark on exciting adventures and solve everyday challenges.",
     },
     {
       title: "Teen Titans",
-      backgroundImage: "https://example.com/images/teen-titans.jpg",
+      backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs-Up1iJnWLCGc3ii4Q4xYjXwPiukR54nZTw&s",
       synopsis: "The Teen Titans band together to protect their city from villains while navigating their own lives.",
   
     },
     {
       title: "Bleach : The Movie",
-      backgroundImage: "https://example.com/images/teen-titans.jpg",
+      backgroundImage: "https://i.ytimg.com/vi/58Txqly_BzM/maxresdefault.jpg",
       synopsis: "Ichigo Kurosaki and his friends battle dangerous spirits and protect the living world while uncovering secrets of the Soul Society.",
 
     },
     {
       title: "Detective Conan",
-      backgroundImage: "https://example.com/images/teen-titans.jpg",
+      backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsf6Sf3huMU2b7AxMV_-hDAKKHjbrRF8X8JQ&s",
       synopsis: "Conan Edogawa solves complex mysteries and brings criminals to justice while navigating life as a kid detective with a hidden identity.",
     },
 ];
