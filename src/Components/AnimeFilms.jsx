2cd576535682ac10cd9b91f239e82d98a3df31f3
MiesieduoCodes <miesieduoveria@gmail.com>
2025-01-12 09:52:48 +0100

    ion care

--- src/Components/AnimeFilms.jsx
+++ src/Components/AnimeFilms.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import { UserContext } from "./UserContext";
